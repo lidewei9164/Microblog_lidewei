220433bc5b1867e848fdcac7446cf185daa13001
lidewei <lidewei@xunlei.com>
2020-07-02 22:00:00 +0800

    NO.4 SQL

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from config import Config
+app =  Flask(__name__)
+app.config.from_object(Config)
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+from app import routes, module
